30e348b88d21363879ce3f99b93d9094f2ed95ec
Nikmaikl <snownikmaikl@gmail.com>
2014-03-16 19:23:01 +0400

    Added sprite system

--- src/game/main/Game.java
+++ src/game/main/Game.java
@@ -17,7 +17,7 @@ public class Game extends Canvas implements Runnable
 {
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 300;
-	public static final int HEIGHT = WIDTH / 16 * 9;
+	public static final int HEIGHT = WIDTH / 4 * 3;
 	public static final int SCALE = 3;
 	public static final String TITLE = "";
 	
